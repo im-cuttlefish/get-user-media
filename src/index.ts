90449f563a5932cf9bf6db85ac08c9f9c81dbbbc
im-cuttlefish <firefly.squid.ink@gmail.com>
2020-05-04 18:51:46 +0900

    fix this

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-const originalGUM = navigator.mediaDevices.getUserMedia;
+const originalGUM = navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices);
 
 navigator.mediaDevices.getUserMedia = async (constraints: MediaStreamConstraints) => {
   const stream = await originalGUM(constraints);
   return stream;
 };
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true });
